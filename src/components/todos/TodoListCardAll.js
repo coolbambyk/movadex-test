97fb2c304c8a503ee8fba6c626f7770a891f88e8
serhiy.yavorskyy <serhiy.yavorskyy@uplab.io>
2021-07-09 07:56:32 +0300

    last commit

--- src/components/todos/TodoListCardAll.js
+++ src/components/todos/TodoListCardAll.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardBody } from 'reactstrap';
 import useLiveData from 'hooks/common/useLiveData';
 import CategoryDataService from 'services/CategoryDataService';
@@ -12,21 +12,57 @@ const useLiveCategories = () => {
   });
 };
 
+const Search = (props) => {
+  return (
+    <input
+      onChange={({ target: { value } }) => props.search(value)}
+      className="form-control"
+      type="text"
+      placeholder="Search here..."
+    />
+  );
+};
+
 const TodoListCard = ({ todos, setActiveStatus, activeStatus }) => {
   const categories = useLiveCategories();
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(
+    (_) => {
+      setFiltered(todos);
+    },
+    [todos],
+  );
+
+  const search = (val) => {
+    let currentTodos = [];
+    let newList = [];
+    if (val !== '') {
+      currentTodos = todos;
+      newList = currentTodos.filter((todo) => {
+        const lc = todo.message.toLowerCase();
+        const filter = val.toLowerCase();
+        return lc.includes(filter);
+      });
+    } else {
+      newList = todos;
+    }
+    setFiltered(newList);
+  };
   return (
     <div>
+      <Search {...{ search }} />
       <Card className="card-tasks">
         <TodoListHeader setActiveStatus={setActiveStatus} activeStatus={activeStatus} />
         <CardBody>
-          <TodoTable todos={todos} />
+          <TodoTable todos={filtered} />
         </CardBody>
       </Card>
       {categories.map((category) => (
         <Card id={category._id} className="card-tasks">
           <TodoListHeader title={`${category.title}`} setActiveStatus={setActiveStatus} activeStatus={activeStatus} />
           <CardBody>
-            <TodoTable todos={todos} category={category._id} />
+            <TodoTable todos={filtered} category={category._id} />
           </CardBody>
         </Card>
       ))}
